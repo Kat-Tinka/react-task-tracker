225a2e98b27727132ade8b9f7336c0794160baf8
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-16 14:08:03 +0200

    destructuring up by using {title} in h1 &Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 
-// If I don't pass anything in >Header/> , I can have default props - but I have to go below the function component an say: Header.defaultProps and set that to an object and set the title to 'Task Tracker!!'
+// If I don't pass anything in >Header/> , I can have default props - but I have to go below the function component and say: Header.defaultProps and set that to an object and set the title to 'Task Tracker!!'
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
   );
 };
 
-// That's the dafault text, but if I put pass something (for example title='YESS') in <Header /> , it would overwrite the dafault text
+// 'Task Tracker' is the dafault text, but if I put pass something (for example title='YESS') in <Header /> , it would overwrite the dafault text
 Header.defaultProps = {
   title: "Task Tracker!!",
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-const Header = (props) => {
+import { upload } from "@testing-library/user-event/dist/upload";
+
+// We can destructure and clean this a bit up, by putting instead of "props" in the const Header whatever you want to pass in -> for Example by adding {}and taking in the 'title' and just call 'title ' in the <h1>
+
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
