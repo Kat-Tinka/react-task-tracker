e663ebc2d91c2e8506dc36aafc6c40c9d9ce3ad1
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-16 13:54:23 +0200

    use default props & overwrite it with HEADER Title

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-// If we want to use a class based component, we do have to import react from 'react' and also call it (same thing) app, but we need to extends react.Component, because we need all the life cycle methods. Then we need a "render" , who take scare of the output and a "return" with an h1/ etc.
-// import React from "react";
-
 import Header from "./components/Header";
+
+// If I don't pass anything in >Header/> , I can have default props - but I have to go below the function component an say: Header.defaultProps and set that to an object and set the title to 'Task Tracker!!'
 
 const App = () => {
   return (
     <div className="container">
-      <Header title="Hello!" />
+      <Header title="YESS" />
     </div>
   );
 };
 
-// class App extends React.Component {
-//   render() {
-//     return <h1>Hello from a class</h1>;
-//   }
-// }
+// That's the dafault text, but if I put pass something (for example title='YESS') in <Header /> , it would overwrite the dafault text
+Header.defaultProps = {
+  title: "Task Tracker!!",
+};
 
 export default App;
