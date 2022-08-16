15e525e9acacd8f2cf4423ea4ee3993a6b775798
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-16 13:39:05 +0200

    back to prev. version <header/>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // If we want to use a class based component, we do have to import react from 'react' and also call it (same thing) app, but we need to extends react.Component, because we need all the life cycle methods. Then we need a "render" , who take scare of the output and a "return" with an h1/ etc.
-import React from "react";
+// import React from "react";
 
 import Header from "./components/Header";
 
-// const App = () => {
-//   return (
-//     <div className="container">
-//       <Header />
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+};
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a class</h1>;
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>;
+//   }
+// }
 
 export default App;
