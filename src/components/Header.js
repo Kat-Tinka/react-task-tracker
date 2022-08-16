b26c7b465f2bef3755c765ee0ea9f904c54342bf
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-16 14:24:16 +0200

    warning-> using title: PropTypes.string.isRequired

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,14 @@ const Header = ({ title }) => {
   );
 };
 
-Header.defaultProps = {
-  title: "Task Tracker",
-};
+// Header.defaultProps = {
+//   title: "Task Tracker",
+// };
 
-// ...and because we know , that we have a title, we use 'title' and we want to create a type of string-> now pass in the <Header/> of App.js 'title={1}-> '1'will be visible, but we will get an error
+// ...and because we know , that we have a title, we use 'title' and we want to create a type of string-> now pass in the <Header/> of App.js 'title={1}-> '1'will be visible, but we will get an error/ warning
+when;
 Header.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
 };
 
 export default Header;
