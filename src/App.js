a4692a8b1fe6e2db4878b9d245132fa385c706d6
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-16 13:15:26 +0200

    write JS directly in function App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
 
 function App() {
+  const name = "Katinka";
   return (
     <div className="container">
-      <Header title="Hello" />
+      <Header title="Hello from" />
+      <h2>Hello {name}</h2>
     </div>
   );
 }
