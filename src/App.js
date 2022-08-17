e0252d1429f5e81fb2e93962c40a36bb002bfe83
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-17 11:25:40 +0200

    task-list, looped over(.map), task component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+//After created a Taskks.js-File and a task list and looped over the tasks ( .map()-Method => output : a jsx=> "list", so teh parent of that list ( in this case <h3> should have a key prop, which should be something unique (go for that back to Tasks.js) )), we import the Tasks to the App.js-file
+import Tasks from "./components/Tasks";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Tasks />
     </div>
   );
 };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,38 @@
-const tasks = [];
+// new file "Tasks.js" created  with a variable called tasks and set that to an array-> paste differents tasks (=> object) with id, text,day, reminder:true/false (boolean value)-> becuae we don't want this to be part od our component, we want this to be part of our state, therefore we will change this later
+// => so we first start here a demo, to loop through these tasks and see the output-> by creating a list ,by using the map().array Method
 
+// TASK LIST:
+const tasks = [
+  {
+    id: 1,
+    text: "Doc Appointment",
+    day: "Aug 17th at 4:30 pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Kingas Birthday",
+    day: "Aug 23th at 8:30 pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Supermarket Shopping",
+    day: "Aug 27th at 9:00 am",
+    reminder: false,
+  },
+];
+
+// =>LOOP through the tasks and see the output-> by creating a list ,by using the MAP().ARRAY METHOD
 const Tasks = () => {
-  return <div></div>;
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
 };
+// =>nothing will be visible yet, because we haven't brought in the task component yet(go to=> App.js and import Tasks)
 
 export default Tasks;
